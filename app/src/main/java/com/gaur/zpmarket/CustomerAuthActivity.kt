71ff969505e45eeed0fc7b640289c8b90ddec5ea
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-09-03 13:51:14 +0530

    New messge

--- app/src/main/java/com/gaur/zpmarket/CustomerAuthActivity.kt
+++ app/src/main/java/com/gaur/zpmarket/CustomerAuthActivity.kt
@@ -1,7 +1,7 @@
 package com.gaur.zpmarket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +9,7 @@ class CustomerAuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_auth)
+
+
     }
 }
